7b744f8f07172a95bb154aab0b39d77362fbb4d8
Gustavo <gustascrows@gmail.com>
2023-03-30 14:01:59 -0300

    Inserindo no bd

---
+++ Banco de dados/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(id string, name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:1234@tcp(localhost:3307)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
