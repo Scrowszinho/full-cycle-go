de7be672c415314206af9f343b68835992cb59fc
Luis Gustavo~ <gustascrows@gmail.com>
2023-04-03 21:17:33 -0300

    Where  e update

--- gorm/main.go
+++ gorm/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -26,7 +24,7 @@ func main() {
 	// 	{Name: "Iphone", Price: 3000},
 	// }
 	// db.Create(&products)
-	var products []Product
+	var products Product
 	// db.First(&products, 1)
 	// fmt.Println(products)
 	// db.First(&products, "name = ?", "Iphone")
@@ -34,6 +32,9 @@ func main() {
 	// db.Limit(2).Offset(2).Find(&products)
 	// fmt.Println(products)
 	// db.Where("price >= ?", 1000).Find(&products)
-	db.Where("name LIKE ?", "%a%").Find(&products)
-	fmt.Println(products)
+	// db.Where("name LIKE ?", "%a%").Find(&products)
+	// db.First(&products, 1)
+	// products.Name = "Mouse"
+	// db.Save(products)
+	// db.Delete(&products)
 }
