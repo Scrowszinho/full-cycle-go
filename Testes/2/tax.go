366a1892358a22ef0c07b5602eb7fcc480aa9e2f
Luis Gustavo~ <gustascrows@gmail.com>
2023-04-17 19:30:11 -0300

    Usando Mocking

--- Testes/2/tax.go
+++ Testes/2/tax.go
@@ -3,8 +3,20 @@ package tax
 func CalcTax(amount float64) float64 {
 	if amount >= 1000 {
 		return 10.0
+	}
+	if amount <= 0 {
+		return 0.0
 	} else {
 		return amount * 0.05
 	}
 
+}
+
+type Repository interface {
+	SaveTax(amount float64) error
+}
+
+func CalculateTaxAndSave(amount float64, repository Repository) error {
+	tax := CalcTax(amount)
+	return repository.SaveTax(tax)
 }
--- Testes/2/tax_test.go
+++ Testes/2/tax_test.go
@@ -1,12 +1,23 @@
 package tax
 
 import (
-	"testing"
-
+	"errors"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestCalcTax(t *testing.T) {
 	tax := CalcTax(1000.0)
 	assert.Equal(t, 10.0, tax)
+}
+
+func TestCalcTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 0.0).Return(errors.New("Error saving tax"))
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	repository.AssertExpectations(t)
+
 }
