2072e846415adc66fa9e432b83d3370d7fa44fff
Luis Gustavo~ <gustascrows@gmail.com>
2023-04-19 20:07:28 -0300

    Configurando usuario

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/Scrowszinho/full-cycle-go/tree/master/apis/configs"
+)
+
 func main() {
 	config, err := configs.LoadConfig(".")
+	println(config.DBDriver)
 }
---
+++ apis/internal/entity/user.go
@@ -0,0 +1,31 @@
+package enity
+
+import (
+	"github.com/Scrowszinho/full-cycle-go/apis/pkg/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Password string    `json:"-"`
+	Email    string    `json:"email"`
+}
+
+func NewUser(name string, email string, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       entity.NewId(),
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}, nil
+}
+
+func (u *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
