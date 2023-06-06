ed7ea765fa108cced62ce8b4eea8de791b0043f6
Scrowszinho <gustascrows@gmail.com>
2023-06-06 20:37:02 -0300

    Consumindo mensagens

--- Eventos/pkg/rabbitmq/rabbitmq.go
+++ Eventos/pkg/rabbitmq/rabbitmq.go
@@ -3,7 +3,7 @@ package rabbitmq
 import amqp "github.com/rabbitmq/amqp091-go"
 
 func OpenChannel() (*amqp.Channel, error) {
-	conn, err := amqp.Dial("amqp//guest:gusest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		panic(err)
 	}
