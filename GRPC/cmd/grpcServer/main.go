91a37d67857805a777415c41db5c1b055c83b962
Scrowszinho <gustascrows@gmail.com>
2023-07-06 20:15:00 -0300

    Criando servidor grpc

---
+++ GRPC/cmd/grpcServer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+	"teste/GRPC/internal/database"
+	"teste/GRPC/internal/pb"
+	"teste/GRPC/internal/service"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryServices(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(listener); err != nil {
+		panic(err)
+	}
+
+}
