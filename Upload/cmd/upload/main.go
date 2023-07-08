693362fc21111ddf38ceae1e6caebaf70469bc26
Scrowszinho <gustascrows@gmail.com>
2023-07-07 22:13:27 -0300

    Criando multicanais

--- Upload/cmd/upload/main.go
+++ Upload/cmd/upload/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -14,6 +15,7 @@ import (
 var (
 	s3Client *s3.S3
 	s3Bucket string
+	wg       sync.WaitGroup
 )
 
 func init() {
@@ -40,6 +42,7 @@ func main() {
 		panic(err)
 	}
 	defer dir.Close()
+	uploadControl := make(chan struct{}, 10)
 	for {
 		files, err := dir.Readdir(10)
 		if err != nil {
@@ -49,16 +52,21 @@ func main() {
 			fmt.Printf("Error reading directory: %s\n", err.Error())
 			continue
 		}
-		uploadFile(files[0].Name())
+		wg.Add(1)
+		uploadControl <- struct{}{}
+		go uploadFile(files[0].Name(), uploadControl)
 	}
+	wg.Wait()
 }
 
-func uploadFile(filename string) {
+func uploadFile(filename string, uploadControl <-chan struct{}) {
+	defer wg.Done()
 	completeFileName := fmt.Sprintf("/tmp/%s", filename)
 	fmt.Printf("Uploding file %s to bucket %s", completeFileName, s3Bucket)
 	f, err := os.Open(completeFileName)
 	if err != nil {
 		fmt.Printf("Error openning file %s\n", completeFileName)
+		<-uploadControl
 		return
 	}
 	defer f.Close()
@@ -70,7 +78,9 @@ func uploadFile(filename string) {
 	})
 	if err != nil {
 		fmt.Printf("Error upload file %s\n", completeFileName)
+		<-uploadControl
 		return
 	}
 	fmt.Printf("File %s upload successful\n", completeFileName)
+	<-uploadControl
 }
