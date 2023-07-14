2a3bcb18c42ca56cd5434c8ed0e45e5052118216
Scrowszinho <gustascrows@gmail.com>
2023-07-13 21:06:20 -0300

    Comecando crud

--- Sqlc/internal/cmd/runSQLC/main.go
+++ Sqlc/internal/cmd/runSQLC/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"context"
+	"database/sql"
+	"teste/sqlc/internal/db"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
 func main() {
+	ctx := context.Background()
+	dbNewConn, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/teste")
+	if err != nil {
+		panic(err)
+	}
+	defer dbNewConn.Close()
+
+	queries := db.New(dbNewConn)
+
+	err = queries.CreateProduct(ctx, db.CreateProductParams{
+		ID:          uuid.New().String(),
+		Name:        "teste",
+		Description: sql.NullString{String: "Teste desc"},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	categories, err := queries.ListProducts(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, category := range categories {
+		println(category.ID, category.Name, category.Description.String)
+	}
 
 }
--- Sqlc/migrations.md
+++ Sqlc/migrations.md
@@ -4,3 +4,4 @@
     migrate create -ext=sql -dir=sql/migrations -seq init
     migrate -path=sql/migrations -database "mysql://root:root@tcp(localhost:3306)/courses" -verbose up
     migrate -path=sql/migrations -database "mysql://root:root@tcp(localhost:3306)/courses" -verbose down
+sqlc generate
