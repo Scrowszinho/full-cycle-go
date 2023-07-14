0e862f9fc1d5b76fcd1d2bccc327f43bec7b5717
Scrowszinho <gustascrows@gmail.com>
2023-07-13 21:15:25 -0300

    Finalizando crud

--- Sqlc/internal/cmd/runSQLC/main.go
+++ Sqlc/internal/cmd/runSQLC/main.go
@@ -6,7 +6,6 @@ import (
 	"teste/sqlc/internal/db"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/google/uuid"
 )
 
 func main() {
@@ -19,20 +18,30 @@ func main() {
 
 	queries := db.New(dbNewConn)
 
-	err = queries.CreateProduct(ctx, db.CreateProductParams{
-		ID:          uuid.New().String(),
-		Name:        "teste",
-		Description: sql.NullString{String: "Teste desc"},
-	})
-	if err != nil {
-		panic(err)
-	}
+	// err = queries.CreateProduct(ctx, db.CreateProductParams{
+	// 	ID:          uuid.New().String(),
+	// 	Name:        "teste",
+	// 	Description: sql.NullString{String: "Teste desc", Valid: true},
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// err = queries.UpdateProduct(ctx, db.UpdateProductParams{
+	// 	Name:        "Teste corrigido",
+	// 	Description: sql.NullString{String: "Teste desc corrigido"},
+	// 	ID:          "cfd7c491-e39f-4b31-9fd9-39c7d42ecccc",
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	err = queries.DeleteProduct(ctx, "2c3addb1-fc98-4339-895e-c3a62f4113d8")
 
 	categories, err := queries.ListProducts(ctx)
 	if err != nil {
 		panic(err)
 	}
-
 	for _, category := range categories {
 		println(category.ID, category.Name, category.Description.String)
 	}
