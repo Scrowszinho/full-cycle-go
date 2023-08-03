eec12f4798b37a3cb14705d98941920aecf7abdf
Gustavo <gustascrows@gmail.com>
2023-08-02 22:03:54 -0300

    Testando DI

---
+++ Dependence Injection/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"teste/product"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		panic(err)
+	}
+
+	repository := product.NewProductRepository(db)
+	usecase := product.NewProductUseCase(repository)
+
+	product, err := usecase.GetProduct(1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(product)
+}
