aaa9ec6ef29981b189b7264f312ab38b0d0f6e1e
Gustavo <gustascrows@gmail.com>
2023-08-03 21:32:16 -0300

    Trabalhando com interfaces e sets

--- Dependence Injection/wire.go
+++ Dependence Injection/wire.go
@@ -9,9 +9,14 @@ import (
 	"teste/product"
 )
 
+var setRepositoryDependency = wire.NewSet(
+	product.NewProductRepository,
+	wire.Bind(new(product.ProductRepositoryInterface), new(*product.ProductRepository)),
+)
+
 func NewUseCase(db *sql.DB) *product.ProductUseCase {
 	wire.Build(
-		product.NewProductRepository,
+		setRepositoryDependency,
 		product.NewProductUseCase,
 	)
 	return &product.ProductUseCase{}
