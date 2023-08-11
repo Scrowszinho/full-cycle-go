c6dcb4c3722ce862aafbca171361773e7b8466dc
Gustavo <gustascrows@gmail.com>
2023-08-11 20:18:54 -0300

    startando server

--- Clean Arc/cmd/system/main.go
+++ Clean Arc/cmd/system/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net"
+	"net/http"
 	"teste/configs"
 	"teste/internal/events"
+	"teste/internal/infra/graph"
+	"teste/internal/infra/web/webserver"
 
 	"github.com/streadway/amqp"
+	"golang.org/x/tools/playground"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -24,6 +31,36 @@ func main() {
 	eventDispatcher.Register("OrderCreated", &handlerOrderCreated{
 		RabbitMQChannel: rabbitMQChannel,
 	})
+
+	createOrderUseCase := NewCreateOrderUseCase(db, eventDispatcher)
+
+	webserver := webserver.NewWebServer(configs.DBPort)
+	webserverHandler := NewWebOrderHandler(db, eventDispatcher)
+	webserver.AddHandler("/order", webserverHandler.Create)
+	fmt.Println("Starting web server")
+	go webserver.Start()
+
+	grpcServer := grpc.NewServer()
+	createOrderService := service.NewOrderService(*createOrderUseCase)
+	pb.RegisterOrderService(grpcServer, createOrderService)
+	reflection.Register(grpcServer)
+	fmt.Sprintln("Grpc server started")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", configs.GRPCHost))
+	if err != nil {
+		panic(err)
+	}
+	go grpcServer.Serve(lis)
+
+	srv := graphql_handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolvers{
+		CreateOrderUseCase: *createOrderUseCase,
+	}}))
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	fmt.Println("Starting GraphQL playground")
+	http.ListenAndServe(":"+configs.GraphQLPort, nil)
+
 }
 
 func getRabbitMQChannel() *amqp.Channel {
